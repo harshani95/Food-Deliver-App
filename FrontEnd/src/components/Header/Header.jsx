b76b98df9a1ebf423895268fb9a641f1fbdc8b90
harshani <ruwanthikapemarathna@gmail.com>
2024-08-18 23:57:22 +0530

    completed header component

---
+++ FrontEnd/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order your favourite food here.</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dises crafted with the  finest ingredients and Cultiarly expertice.
+                Our misson is satisfy your cravings and elevate your dining experience, one deliciouse meal at a time. </p>
+            <button>-View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
